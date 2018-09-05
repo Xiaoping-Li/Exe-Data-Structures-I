2f2b1c5993cf612357f70b9798c927f6c705c3c5
Xiaoping Li <narci2008@gmail.com>
2018-09-05 16:36:47 -0700

    Add addToTail(value) to double-linked-list

--- src/doubly-linked-list.js
+++ src/doubly-linked-list.js
@@ -18,4 +18,19 @@ class DoublyLinkedList {
     newNode.next = this.head;
     this.head = newNode;
   }
+
+  addToTail(value) {
+    const newNode = {
+      value,
+      prev: null,
+      next: null,
+    };
+    if (!this.tail) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    this.tail.next = newNode;
+    newNode.prev = this.tail;
+    this.tail = newNode;
+  }
 }
