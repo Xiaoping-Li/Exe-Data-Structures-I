a0e61974b10eb74c14b3bbc8fd352f283a763b20
Xiaoping Li <narci2008@gmail.com>
2018-09-05 16:31:16 -0700

    Add addToHead() to double-linked-list

--- src/doubly-linked-list.js
+++ src/doubly-linked-list.js
@@ -3,4 +3,19 @@ class DoublyLinkedList {
     this.head = null;
     this.tail = null;
   }
+
+  addToHead(value) {
+    const newNode = {
+      value,
+      prev: null,
+      next: null,
+    };
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    this.head.prev = newNode;
+    newNode.next = this.head;
+    this.head = newNode;
+  }
 }
